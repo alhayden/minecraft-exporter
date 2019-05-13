b15aec36db20ce9ede40aac28f52602661302679
Alexander Hayden <alexhayden25@gmail.com>
2019-05-13 19:23:14 -0400

    COMMENTS

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Prometheus exporter for minecraft statistics
 
 ## Use
 Requires `prometheus_client` which can be installed via `pip` or from [the project's github page](https://github.com/prometheus/client_python)
-Simply run `minecraft-exporter.py`, which will continually export metrics to the host's 8000 port until the program is stopped.
+
+
+To start exporting metrics, simply run `minecraft-exporter.py`, which will continually export metrics to the host's 8000 port until the program is stopped.
--- minecraft-exporter.py
+++ minecraft-exporter.py
@@ -3,26 +3,35 @@ from prometheus_client import start_http_server
 import json, os
 from signal import pause
 
+# replace this with the path to the world which you intend to export metrics from
 STATS_DIR='PATH-TO-WORLD-SAVE/stats'
 
 class MinecraftMetricCollector(object):
     def collect(self):
+        # finds the stats file for each player
         files = os.listdir(STATS_DIR)
+        # create global stats storage
         stats = {}
+        # loop through players
         for f in files:
             player = json.loads(open(STATS_DIR + '/' + f,'r').read())['stats']
             for k1 in player.keys():
                 if not k1 in stats.keys():
+                    # if a stat is not already in the global list, add it
                     stats[k1] = {}
+                    # add an 'all' subcategory if one does not exist already
                     if not 'all' in stats[k1]:
                         stats[k1]['all'] = 0
                 for k2 in player[k1]:
+                    # add stat to global list if not already there
                     if k2 not in stats[k1].keys():
                         stats[k1][k2] = 0
+                    # sum stat with global list
                     stats[k1][k2] += player[k1][k2]
                     stats[k1]['all'] += player[k1][k2]
 
 
+        # generate exports
         for k1 in stats.keys():
             for k2 in stats[k1].keys():
                 name = 'minecraft_' + k1 + '_' + k2
@@ -34,5 +43,5 @@ REGISTRY.register(MinecraftMetricCollector())
 
 start_http_server(8000)
 
-
+# suspend the program indefinitely so the exporter can run in the background
 pause()
